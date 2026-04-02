6dfe92308bfc4681561f924f74f3d229388a49de
ezrakibbs <ezrazeleke@gmail.com>
2026-04-02 16:32:24 -0700

    added IF_STAGE to top.v - takes clk, reset, input instruction as inputs - output instruction is its only output but might need to output PC later - have combinational logic to initialize instruction memory
    added TB_IF_STAGE to testbench.v
    - generate clock and reset
    - feed instructions in one by one and use 1ns delay between each instruction change

--- testbench.v
+++ testbench.v
@@ -1,6 +1,55 @@
 `timescale 1ns / 1ps
 
-module testbench;
+module TB_IF_STAGE;
+    reg clk;
+    reg reset;
+    reg [31:0] input_instruction;
+    wire [31:0] output_instruction;
+    reg done_initializing_instructions;
+
+
+    IF_STAGE test(
+        .clk_x61(clk),
+        .reset_x61(reset),
+        .input_instruction_x61(input_instruction), 
+        .output_instruction_x61(output_instruction));
+
+    // clk generation
+    initial
+    begin
+        clk = 0;
+        reset = 1;
+        #5
+        reset = 0;
+    end
+
+    always 
+    begin
+        #10
+        if(done_initializing_instructions)
+            clk <= ~clk;
+    end
+
+    // instruction memory initialization
+    initial
+    begin
+        done_initializing_instructions = 0;
+        #5 input_instruction = 32'b000000_00000_00000_00001_00000_100001;
+        #1 input_instruction = 32'b000100_00111_00000_0000_0000_0010_0000;
+        #1 input_instruction = 32'b100011_00011_00010_0000_0000_0000_0000;
+        #1 input_instruction = 32'b100011_00101_00100_0000_0000_0000_0000;
+        #1 input_instruction = 32'b000000_00010_00100_00010_00000_011000;
+        #1 input_instruction = 32'b000000_00001_00010_00001_00000_100001;
+        #1 input_instruction = 32'b001001_00011_00011_0000_0000_0000_0100;
+        #1 input_instruction = 32'b001001_00101_00101_0000_0000_0000_0100;
+        #1 input_instruction = 32'b001001_00111_00111_1111_1111_1111_1111;
+        #1 input_instruction = 32'b000010_00_0000_0000_0000_0000_0000_1000;
+        #1 input_instruction = 32'b000000_11111_000_0000_0000_0000_001000;
+        #1 done_initializing_instructions = 1;
+        
+        #300
+        $finish;
+    end
 
 endmodule
 
--- top.v
+++ top.v
@@ -1,10 +1,58 @@
 `timescale 1ns / 1ps
 
-module ();
+module IF_STAGE(clk_x61, reset_x61, input_instruction_x61, output_instruction_x61);
+  input wire clk_x61;  
+  input wire reset_x61;
+  input wire [31:0] input_instruction_x61;
+  output reg [31:0] output_instruction_x61;
 
-  always @(posedge clk)
+
+  localparam max_instruction_count = 11;
+  
+  reg [7:0] instr_mem_x61 [43:0]; // 11instr x 4bytes/instr = 44bytes; byte addressable
+  reg [3:0] write_index_x61;
+  integer i;
+  always @(*)
   begin
-
+    if(reset_x61)
+    begin
+      write_index_x61 = 0;
+      for(i = 0; i < 44; i= i +1)
+      begin
+        instr_mem_x61[i] = 8'b0000_0000;
+      end
+    end
+    
+    else if(write_index_x61 < max_instruction_count)
+    begin
+      instr_mem_x61[write_index_x61*4 + 0] = input_instruction_x61[7:0];
+      instr_mem_x61[write_index_x61*4 + 1] = input_instruction_x61[15:8];
+      instr_mem_x61[write_index_x61*4 + 2] = input_instruction_x61[23:16];
+      instr_mem_x61[write_index_x61*4 + 3] = input_instruction_x61[31:24];
+      write_index_x61 = write_index_x61 + 1;
+    end
   end
 
+  reg [5:0] next_program_counter_x61;
+  always @(*)
+  begin
+    if(reset_x61)
+    begin
+      next_program_counter_x61 = 0;
+    end
+    else
+    begin
+      next_program_counter_x61 = (program_counter_x61 < 4*max_instruction_count - 4) ? program_counter_x61 + 4: program_counter_x61;
+    end
+  end
+
+  reg [5:0] program_counter_x61;
+  always @(posedge clk_x61 or posedge reset_x61)
+  begin
+    program_counter_x61 <= next_program_counter_x61;
+    output_instruction_x61 <= {instr_mem_x61[program_counter_x61 + 3],
+                               instr_mem_x61[program_counter_x61 + 2],
+                               instr_mem_x61[program_counter_x61 + 1],
+                               instr_mem_x61[program_counter_x61 + 0]};
+  end
 endmodule
